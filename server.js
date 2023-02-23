67a4ccafa1520605dce4a4e18746899908bbba28
Jakub Štellner <stelljak@fit.cvut.cz>
2023-02-23 16:38:21 +0100

    Rework ssr

--- server.js
+++ server.js
@@ -1,42 +1,66 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'url';
-import express from 'express';
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'url'
+import express from 'express'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const app = express()
 
-export async function createServer() {
-  const resolve = (p) => path.resolve(__dirname, p);
+export async function createServer(
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === 'production',
+  hmrPort
+) {
+  const resolve = (p) => path.resolve(__dirname, p)
 
-  let vite = null;
-
-  app.use((await import('compression')).default());
-  app.use(
-    (await import('serve-static')).default(resolve('dist/client'), {
-      index: false,
-    }),
-  );
+  let vite = null
+  if (!isProd) {
+    vite = await (
+      await import('vite')
+    ).createServer({
+      root,
+      logLevel: 'info',
+      server: {
+        middlewareMode: true,
+        hmr: {
+          port: hmrPort
+        }
+      },
+      appType: 'custom'
+    })
+    app.use(vite.middlewares)
+  } else {
+    app.use((await import('compression')).default())
+    app.use(
+      (await import('serve-static')).default(resolve('dist/client'), {
+        index: false
+      })
+    )
+  }
 
   app.use('*', async (req, res) => {
-    const url = '/';
+    const url = '/'
+    let template, render
+    if (!isProd && vite) {
+      template = fs.readFileSync(resolve('index.html'), 'utf-8')
+      template = await vite.transformIndexHtml(url, template) // Inserting react-refresh for local development
+      render = (await vite.ssrLoadModule('/src/entry-server.tsx')).SSRRender
+    } else {
+      template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+      render = (await import('./dist/server/entry-server.js')).SSRRender
+    }
 
-    const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8');
-    const render = (await import('./dist/server/entry-server.js')).SSRRender;
+    const appHtml = render(url) //Rendering component without any client side logic de-hydrated like a dry sponge
+    const html = template.replace(`<!--app-html-->`, appHtml) //Replacing placeholder with SSR rendered components
 
-    const appHtml = render(url); //Rendering component without any client side logic de-hydrated like a dry sponge
-    const html = template.replace(`<!--app-html-->`, appHtml); //Replacing placeholder with SSR rendered components
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html) //Outputing final html
+  })
 
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html); //Outputing final html
-  });
-
-  return { app, vite };
+  return { app, vite }
 }
 
-const port = process.env.PORT || 3000;
-
 createServer().then(({ app }) =>
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  }),
-);
+  app.listen(5173, () => {
+    console.log('http://localhost:5173')
+  })
+)
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './App';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
 
 ReactDOM.hydrateRoot(
   document.getElementById('app')!,
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-);
+  </BrowserRouter>
+)
